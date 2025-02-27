0fd9185ed0af5b92cb184eb3f251e7a8539fcacd
TecoMinaC4ma <henrique.dornelas@unesp.br>
2025-02-27 01:06:28 -0300

    consertando umas paradas

--- CaixaBancario/Form1.Designer.cs
+++ CaixaBancario/Form1.Designer.cs
@@ -36,7 +36,10 @@
             numLimite = new NumericUpDown();
             btDepositar = new Button();
             btSacar = new Button();
-            lblDepErro = new Label();
+            lblErro = new Label();
+            btLimparValor = new Button();
+            label3 = new Label();
+            lblLimiteSaque = new Label();
             ((System.ComponentModel.ISupportInitialize)numValor).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numLimite).BeginInit();
             SuspendLayout();
@@ -44,47 +47,51 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 18F);
-            label1.Location = new Point(31, 36);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(101, 147);
             label1.Name = "label1";
-            label1.Size = new Size(72, 32);
+            label1.Size = new Size(81, 32);
             label1.TabIndex = 0;
             label1.Text = "Valor:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 18F);
-            label2.Location = new Point(50, 215);
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(140, 381);
             label2.Name = "label2";
-            label2.Size = new Size(78, 32);
+            label2.Size = new Size(84, 32);
             label2.TabIndex = 1;
             label2.Text = "Saldo:";
             // 
             // lblSaldo
             // 
             lblSaldo.AutoSize = true;
-            lblSaldo.Font = new Font("Segoe UI", 15F);
-            lblSaldo.Location = new Point(186, 215);
+            lblSaldo.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            lblSaldo.ForeColor = Color.White;
+            lblSaldo.Location = new Point(225, 384);
             lblSaldo.Name = "lblSaldo";
-            lblSaldo.Size = new Size(77, 28);
+            lblSaldo.Size = new Size(84, 28);
             lblSaldo.TabIndex = 2;
             lblSaldo.Text = "R$ 0,00";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 18F);
-            label4.Location = new Point(31, 278);
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(140, 437);
             label4.Name = "label4";
-            label4.Size = new Size(84, 32);
+            label4.Size = new Size(91, 32);
             label4.TabIndex = 3;
             label4.Text = "Limite:";
             // 
             // numValor
             // 
             numValor.Font = new Font("Segoe UI", 18F);
-            numValor.Location = new Point(116, 32);
+            numValor.Location = new Point(186, 143);
             numValor.Margin = new Padding(3, 2, 3, 2);
             numValor.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
             numValor.Name = "numValor";
@@ -94,7 +101,7 @@
             // numLimite
             // 
             numLimite.Font = new Font("Segoe UI", 18F);
-            numLimite.Location = new Point(128, 278);
+            numLimite.Location = new Point(237, 437);
             numLimite.Margin = new Padding(3, 2, 3, 2);
             numLimite.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
             numLimite.Minimum = new decimal(new int[] { 100000000, 0, 0, int.MinValue });
@@ -104,42 +111,90 @@
             // 
             // btDepositar
             // 
+            btDepositar.BackColor = Color.OrangeRed;
             btDepositar.Font = new Font("Segoe UI", 18F);
-            btDepositar.Location = new Point(61, 107);
+            btDepositar.ForeColor = Color.White;
+            btDepositar.Location = new Point(101, 217);
             btDepositar.Margin = new Padding(3, 2, 3, 2);
             btDepositar.Name = "btDepositar";
             btDepositar.Size = new Size(158, 40);
             btDepositar.TabIndex = 6;
             btDepositar.Text = "Depositar";
-            btDepositar.UseVisualStyleBackColor = true;
+            btDepositar.UseVisualStyleBackColor = false;
             btDepositar.Click += btDepositar_Click;
             // 
             // btSacar
             // 
+            btSacar.BackColor = Color.OrangeRed;
             btSacar.Font = new Font("Segoe UI", 18F);
-            btSacar.Location = new Point(249, 107);
+            btSacar.ForeColor = Color.White;
+            btSacar.Location = new Point(265, 217);
             btSacar.Margin = new Padding(3, 2, 3, 2);
             btSacar.Name = "btSacar";
             btSacar.Size = new Size(158, 40);
             btSacar.TabIndex = 7;
             btSacar.Text = "Sacar";
-            btSacar.UseVisualStyleBackColor = true;
+            btSacar.UseVisualStyleBackColor = false;
+            btSacar.Click += btSacar_Click;
             // 
-            // lblDepErro
+            // lblErro
             // 
-            lblDepErro.AutoSize = true;
-            lblDepErro.Font = new Font("Segoe UI", 12F);
-            lblDepErro.Location = new Point(31, 166);
-            lblDepErro.Name = "lblDepErro";
-            lblDepErro.Size = new Size(0, 21);
-            lblDepErro.TabIndex = 8;
+            lblErro.AutoSize = true;
+            lblErro.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblErro.ForeColor = Color.Red;
+            lblErro.Location = new Point(101, 269);
+            lblErro.Name = "lblErro";
+            lblErro.Size = new Size(108, 21);
+            lblErro.TabIndex = 8;
+            lblErro.Text = "caixa de erro";
+            lblErro.Visible = false;
+            // 
+            // btLimparValor
+            // 
+            btLimparValor.BackColor = Color.DarkRed;
+            btLimparValor.Font = new Font("Segoe UI", 12F);
+            btLimparValor.ForeColor = Color.White;
+            btLimparValor.Location = new Point(323, 147);
+            btLimparValor.Name = "btLimparValor";
+            btLimparValor.Size = new Size(75, 33);
+            btLimparValor.TabIndex = 9;
+            btLimparValor.Text = "Zerar";
+            btLimparValor.UseVisualStyleBackColor = false;
+            btLimparValor.Click += btZerarValor_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Segoe UI", 38F, FontStyle.Bold | FontStyle.Italic);
+            label3.ForeColor = Color.Firebrick;
+            label3.Location = new Point(37, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(462, 68);
+            label3.TabIndex = 10;
+            label3.Text = "CAIXA BANCÁRIO";
+            // 
+            // lblLimiteSaque
+            // 
+            lblLimiteSaque.AutoSize = true;
+            lblLimiteSaque.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblLimiteSaque.ForeColor = Color.LightCoral;
+            lblLimiteSaque.Location = new Point(144, 492);
+            lblLimiteSaque.Name = "lblLimiteSaque";
+            lblLimiteSaque.Size = new Size(165, 21);
+            lblLimiteSaque.TabIndex = 11;
+            lblLimiteSaque.Text = "(Limite para Sacar : )";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
-            Controls.Add(lblDepErro);
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(533, 573);
+            Controls.Add(lblLimiteSaque);
+            Controls.Add(label3);
+            Controls.Add(btLimparValor);
+            Controls.Add(lblErro);
             Controls.Add(btSacar);
             Controls.Add(btDepositar);
             Controls.Add(numLimite);
@@ -167,6 +222,9 @@
         private NumericUpDown numLimite;
         private Button btDepositar;
         private Button btSacar;
-        private Label lblDepErro;
+        private Label lblErro;
+        private Button btLimparValor;
+        private Label label3;
+        private Label lblLimiteSaque;
     }
 }
--- CaixaBancario/Form1.cs
+++ CaixaBancario/Form1.cs
@@ -2,45 +2,87 @@ namespace CaixaBancario
 {
     public partial class Form1 : Form
     {
-        // variável global
-        double saldo = 0;
+        /* ---------- VARIÁVEIS GLOBAIS ---------- */
+
+        double saldo = 500, limite = 300;
+
+        /* ---------- INICIALIZAÇÕES ---------- */
 
         public Form1()
         {
             InitializeComponent();
+
+            if (saldo > 0) lblSaldo.ForeColor = Color.LightGreen;
+            else lblSaldo.ForeColor = Color.Red;
+
+            lblSaldo.Text = saldo.ToString("c");
+            numLimite.Value = (decimal)limite;
+            
+            AtualizarSaldo(saldo);
         }
 
         /* ---------- FUNÇÕES ---------- */
 
         // função para alterar o valor exibido do saldo
-        void AtualizarSaldo(double n)
+        public void AtualizarSaldo(double n)
         {
             lblSaldo.Text = n.ToString("c");
+
+            // saldo
+            if (n > 0) lblSaldo.ForeColor = Color.LightGreen;
+            else lblSaldo.ForeColor = Color.Red;
+
+            // texto limite real
+            double limiteSaque = 0;
+            if (saldo >= 0) limiteSaque = saldo + limite;
+            else limiteSaque = limite + saldo;
+            lblLimiteSaque.Text = $"(Limite para Sacar : {limiteSaque.ToString("c")})";
         }
 
         /* ---------- PROGRAMA ---------- */
 
         private void btDepositar_Click(object sender, EventArgs e)
         {
-            if (numValor.Value > 0) saldo = saldo + (double)numValor.Value;
-            else lblDepErro.Text = "O valor depositado deve ser maior que 0";
+            if (numValor.Value > 0) saldo += (double)numValor.Value;
+            else
+            {
+                lblErro.Visible = true;
+                lblErro.Text = "O valor depositado deve ser maior que 0";
+            }
 
             AtualizarSaldo(saldo);
         }
 
         private void btSacar_Click(object sender, EventArgs e)
         {
-            //Saldo > 0 
             if (numValor.Value > 0)
             {
-                //numValor tirar do saldo
-                saldo = saldo - (double)numValor.Value;
-                //numValor que tirar de saldo não pode ser < que o limite
-                if (saldo > (double)numLimite.Value)
+                double novoSaldo = saldo - (double)numValor.Value;
+
+                // Permite o saque se o novo saldo não for menor que -limite
+                if (novoSaldo >= -limite)
                 {
-                    lblSaldo.Text = saldo.ToString("c");
+                    saldo = novoSaldo; // atualiza o saldo
+                    AtualizarSaldo(saldo);
+                    lblErro.Text = "";
+                }
+                else
+                {
+                    lblErro.Visible = true;
+                    lblErro.Text = "Saldo insuficiente!\nVocê atingiu o limite de saque.";
                 }
             }
+            else
+            {
+                lblErro.Visible = true;
+                lblErro.Text = "O valor do saque deve ser maior que 0!";
+            }
+        }
+
+        private void btZerarValor_Click(object sender, EventArgs e)
+        {
+            numValor.Value = 0;
+            lblErro.Text = "";
         }
     }
 }
